b345b54307b193e24032411837f102dbb5433c26
sato yoshiyuki <yoshi.389111@gmail.com>
2023-05-23 10:14:06 +0900

    :recycle: refact: rewriting `substr`

--- src/dq2utils.ts
+++ src/dq2utils.ts
@@ -6,6 +6,6 @@ export const clipboardCopy = async (text: string): Promise<void> => {
 
 /** 16進数2桁に編集(0xHH形式) */
 export const toHex2 = (value: number) => {
-    return '0x' + (('0' + value.toString(16)).toUpperCase().substr(-2));
+    return '0x' + value.toString(16).toUpperCase().padStart(2, '0');
 }
 
